a42eefe472e9cf56b437eedb89950dcbb148c7f9
Vishaal19fl <vishaalkrish6@gmail.com>
2024-07-12 17:57:33 +0530

    change

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,13 +43,7 @@ window.addEventListener("scroll", function () {
  * go top
  */
 
-const goTopBtn = document.querySelector("[data-go-top]");
 
-window.addEventListener("scroll", function () {
-
-  window.scrollY >= 500 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active");
-
-});
 
 
 function isInViewport(element) {
